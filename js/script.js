e83948c9378f4aee77d366dc260f34dca31b44f9
MichaelScarantino <scarantino.michael@outlook.it>
2021-12-01 13:00:04 +0100

    correct exercise

--- js/script.js
+++ js/script.js
@@ -1,24 +1,12 @@
 // Visualizzare con degli alert 5 numeri casuali. 
-// creo un array vuoto
-let numbersArray = [];
-// credo una variabile per ogni numero 
-const firstNumber = getRndInteger(1, 100);
-alert(firstNumber);
-const secondNumber = getRndInteger(1, 100);
-alert(secondNumber);
-const thirdNumber = getRndInteger(1, 100);
-alert(thirdNumber);
-const fourthNumber = getRndInteger(1, 100);
-alert(fourthNumber);
-const fifthNumber = getRndInteger(1, 100);
-alert(fifthNumber);
-
-// pusho i cinque numeri all'interno dell' array vuoto
-numbersArray.push(firstNumber);
-numbersArray.push(secondNumber);
-numbersArray.push(thirdNumber);
-numbersArray.push(fourthNumber);
-numbersArray.push(fifthNumber);
+// creo un array di 5 numeri random diversi
+const numbersGame = 5;
+const numbersRandom = getRandomNumberArray(numbersGame);
+console.log(numbersRandom);
+// visualizzo i numeri in un alert
+numbersRandom.forEach(element => {
+    alert(element);
+});
 
 // parte un timer di 30 secondi.
 const clock = setTimeout(function() {
@@ -26,23 +14,40 @@ const clock = setTimeout(function() {
     // creo un array vuoto per inserire i numeri corretti dell'utente
     const userArray = [];
     // eseguo un ciclo for per chiedere i numeri all'utente
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < numbersGame; i++){
         // per cinque volte l'utente inserisce un numero
         const userNumber = parseInt( prompt('Inserisci un numero tra quelli visualizzati in precedenza') );
         // se il numero è compreso nell'array contenente i numeri assegnati dal pc e non si ripete, lo pusho nell'array userArray
-        if( (numbersArray.includes(userNumber)) && (!userArray.includes(userNumber)) ) {
+        if( (numbersRandom.includes(userNumber)) && (!userArray.includes(userNumber)) ) {
            
             userArray.push(userNumber);
            
         }
     }
     // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-    alert(`Hai indovinato ${userArray.length} numero/i, i numeri indovinati sono: ${userArray}`);
-},30000);
+    if(userArray.length > 1) {
+        alert(`Hai indovinato ${userArray.length} numeri, i numeri indovinati sono: ${userArray}`);
+    } else {
+        alert(`Hai indovinato ${userArray.length} numero, il numero indovinato è: ${userArray}`);
+    }
+    
+},3000);
 
 // -------------
 // FUNCTIONS
 // -------------
+// funzione che genera un array di numeri casuali non ripetuti
+function getRandomNumberArray(numberOfItems){
+    let numbersArray = [];
+    while(numbersArray.length < numberOfItems) {
+        const newNumber = getRndInteger(1, 100);
+        if(!numbersArray.includes(newNumber)) {
+            numbersArray.push(newNumber);
+        }
+    }
+    return numbersArray;
+}
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
